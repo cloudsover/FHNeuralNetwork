ae0063df9942281e9c68b32e15c4c81ba04bfbd5
Ben Kaliczak <cloudsoverozea@gmail.com>
2019-01-11 22:13:37 -0800

    Added framework for next step of the NNData class.

--- NNData.py
+++ NNData.py
@@ -79,8 +79,73 @@ class NNData:
 
     def load_data(self):
         """ Method-stub placeholder"""
+        # TODO check that the lengths of x any y are the same otherwise
+        #  raise datamismatch
+
+        # TODO Assign x and y to the corresponding internal objects self.x
+        #  and self.y
+
+        # TODO Call the method split_set()
         pass
 
+    def split_set(self, new_train_percentage=None):
+        # TODO Docs
+
+        # TODO reassign self.train_percentage if new_train_percentage is not
+        #  None. -> Use the limiter
+
+        # TODO Calculate the size of the training set and testing set using
+        #  self.train_percentage and the size of the loaded data
+
+        # TODO populate train_indices and test_indices with appropriately
+        #  sized lists of indices to examine the data. The indices should be
+        #  assigned randomly
+
+        # TODO Call method prime_data()
+        pass
+
+    def prime_data(self, my_set=None, order=None):
+        # TODO Docs
+
+        # TODO if order is None, set order = NNData.Order.SEQUENTIAL.
+
+        # TODO Copy test_indices to test_pool and train_indices to
+        #  train_pool (or just one if my_set is specified as either Set.TEST
+        #  or Set.TRAIN) Both test pools should be dequeues. If
+        #  order=Order.RANDOM, the indices should be randomized as they are
+        #  copied. Otherwise they should remain in their original order.
+        pass
+
+    def empty_pool(self, my_set=None):
+        # TODO Docs
+
+        # TODO If my_set is None, set my_set = NNData.Set.TRAIN.
+
+        # TODO Return True if the pool indicated by my_set is empty,
+        #  or False if not.
+        pass
+
+    def get_number_samples(self, my_set=None):
+        # TODO Docs
+
+        # TODO If my_set is None, return the total number of samples in the
+        #  dataset.
+
+        # TODO Otherwise, return the total number of samples in the set
+        #  requested (TEST or TRAIN)
+
+        pass
+
+    def get_one_item(self, my_set=None):
+        # TODO Docs
+
+        # TODO if my_set is None, set my_set = NNData.Set.Train.
+
+        # TODO popleft and return one item from the appropriate pool
+        #  dequeue, or None if the requested pool is empty. The item
+        #  returned should be a list of the form [x,y] where x is an example
+        #  and y is the corresponding label (expected result)
+        pass
     # Inner Order Class ------------------------------------------------------
     class Order(Enum):
         """Enum which determines whether the training data is presented in
@@ -113,48 +178,81 @@ class DataMismatchError(Exception):
     pass
 
 
+# Unit Test
 def main():
     errors = False
-    test = NNData()
-    test2 = NNData(percentage=110)
-    test3 = NNData(percentage=-10)
-    test4 = NNData(percentage=30)
     try:
-        assert isinstance(test.Order.RANDOM, Enum)
-        assert isinstance(test.Order.SEQUENTIAL, Enum)
-        assert isinstance(test.Set.TRAIN, Enum)
-        assert isinstance(test.Set.TEST, Enum)
-    except:
-        print("Enums are not properly established")
-        errors = True
-    try:
-        assert test.train_percentage == 100
-        assert test2.train_percentage == 100
-        assert test3.train_percentage == 0
-        assert test4.train_percentage == 30
-    except:
-        print("train_percentage not properly handled")
-        errors = True
-    try:
+        X = list(range(10))
+        Y = X
+        our_data = NNData(X, Y)
+        X = list(range(100))
+        Y = X
+        our_big_data = NNData(X, Y, 50)
+        Y = [1]
         try:
-            raise DataMismatchError
+            our_bad_data = NNData(X, Y)
+            raise Exception
         except DataMismatchError:
             pass
         except:
-            print("DataMismatchError not properly established")
-            errors = True
+            raise Exception
+        X = ['a', 'b', 'c', 'd']
+        Y = ['A', 'B', 'C', 'D']
+        our_char_data = NNData(X, Y, 50)
     except:
-        print("DataMismatchError not properly established")
+        print("There are errors that likely come from __init__ or a method called by __init__")
+        errors = True
+    try:
+        our_data.split_set(30)
+        assert len(our_data.train_indices) == 3
+        assert len(our_data.test_indices) == 7
+        assert (list(set(our_data.train_indices + our_data.test_indices))) == list(range(10))
+    except:
+        print("There are errors that likely come from split_set")
+        errors = True
+    try:
+        our_data.prime_data(order=NNData.Order.SEQUENTIAL)
+        assert len(our_data.train_pool) == 3
+        assert len(our_data.test_pool) == 7
+        assert our_data.train_indices == list(our_data.train_pool)
+        assert our_data.test_indices == list(our_data.test_pool)
+        our_big_data.prime_data(order=NNData.Order.RANDOM)
+        assert our_big_data.train_indices != list(our_big_data.train_pool)
+        assert our_big_data.test_indices != list(our_big_data.test_pool)
+    except:
+        print("There are errors that likely come from prime_data")
+        errors = True
+
+    try:
+        our_data.prime_data(order=NNData.Order.SEQUENTIAL)
+        my_x_list = []
+        my_y_list = []
+        while not our_char_data.empty_pool():
+            example = our_char_data.get_one_item()
+            my_x_list.append(example[0])
+            my_y_list.append(example[1])
+        assert len(my_x_list) == 2
+        assert my_x_list != my_y_list
+        my_upper_x_list = [k.upper() for k in my_y_list]
+        assert my_upper_x_list == my_y_list
+        while not our_char_data.empty_pool(our_char_data.Set.TEST):
+            example = our_char_data.get_one_item(our_char_data.Set.TEST)
+            my_x_list.append(example[0])
+            my_y_list.append(example[1])
+        assert my_x_list != my_y_list
+        my_upper_x_list = [k.upper() for k in my_y_list]
+        assert my_upper_x_list == my_y_list
+        assert set(my_x_list) == set(X)
+        assert set(my_y_list) == set(Y)
+    except:
+        print("There are errors that may come from prime_data, but could be from another method")
         errors = True
     if errors:
-        print(
-            "You have one or more errors.  Please fix them before submitting")
+        print("You have one or more errors.  Please fix them before submitting")
     else:
         print("No errors were identified by the unit test.")
         print("You should still double check that your code meets spec.")
-        print("You should also check that PyCharm does not identify any "
-              "PEP-8 issues.")
-
+        print("You should also check that PyCharm does not identify any PEP-8 issues.")
 
 if __name__ == "__main__":
     main()
