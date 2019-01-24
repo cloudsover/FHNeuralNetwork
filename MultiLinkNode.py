2d71de0befeec111ea15f4224b605ab735e94595
Ben Kaliczak <cloudsoverozea@gmail.com>
2019-01-24 10:34:18 -0800

    CS 3B Assignment 4: Framework of classes & assignment done

---
+++ MultiLinkNode.py
@@ -0,0 +1,50 @@
+from enum import Enum, auto
+
+
+class MultiLinkNode:
+    """TODO DOCS"""
+
+    def __init__(self):
+        """TODO DOCS"""
+
+        # TODO reporting_inputs
+        # TODO reporting_outputs
+        # TODO compare_inputs_full
+        # TODO compare_outputs_full
+        # TODO input_nodes
+        # TODO output_nodes
+        pass
+
+    # fixme
+    def process_new_input_node(self, node):
+        """TODO Docs"""
+        pass
+
+    # fixme
+    def process_new_output_node(self, node):
+        """TODO DOCS"""
+        pass
+
+    # fixme
+    def clear_and_add_input_node(self, nodes):
+        """TODO DOcs"""
+        pass
+
+
+    pass
+
+class LayerType(Enum):
+    """TODO DOCS"""
+    INPUT = auto()
+    HIDDEN = auto()
+    OUTPUT = auto()
+
+# FIXME
+class Neurode:
+    """TODO DOCS"""
+
+    def __init__(self, my_type):
+        """TODO DOCS"""
+        pass
+
+    pass
