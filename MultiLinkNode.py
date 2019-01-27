e45c36317c757a5433f187cb0eab29e73406db6b
Ben Kaliczak <cloudsoverozea@gmail.com>
2019-01-26 16:44:58 -0800

    CS 3B Assignment 4: Finished

--- MultiLinkNode.py
+++ MultiLinkNode.py
@@ -1,91 +1,252 @@
-from abc import *
+from abc import ABC, abstractmethod
+from collections import OrderedDict
 from enum import Enum, auto
+import random
 
 
 class MultiLinkNode(ABC):
-    """TODO DOCS"""
+    """
+    Abstract class which sets up the framework for different types of nodes
+    to be used in the neural network.
+
+    Attributes:
+        input_connections: int representing the number of current input
+          connections the node has
+
+        output_connections: int representing the number of current output
+          connections the node has
+
+        reporting_inputs: binary-encoded representation of input nodes which
+          have provided input to this node
+
+        reporting_outputs: binary-encoded representation of output nodes which
+          have provided input to this node
+
+        compare_inputs_full: binary-encoded representation of all input nodes
+          reporting 'full' to be used to compare against reporting_inputs
+
+        compare_outputs_full: binary-encoded representation of all ouput nodes
+          reporting 'full' to be used to compare against reporting_outputs
+
+        input_nodes: Ordered dictionary of input nodes and corresponding
+          their weights
+
+        output_nodes: Ordered dictionary of input nodes and corresponding
+          their weights
+    """
 
     def __init__(self):
-        """TODO DOCS"""
+        """Inits MultiLinkNode with all class attributes initialized."""
 
-        # TODO reporting_inputs
-        self.input_connections = None
-        self.output_connections = None
-        self.reporting_inputs = []  # Binary
-        self.reporting_outputs = []  # Binary
-        self.compare_inputs_full = []  # Binary
-        self.compare_outputs_full = []  # Binary
-        self.input_nodes = None  # Ordered Dictionary
-        self.output_nodes = None  # Ordered Dictionary
+        self.input_connections: int = 0  # number of input connections
+        self.output_connections: int = 0  # number of output connections
+        self.reporting_inputs: int = 0  # Binary which inputs gave info
+        self.reporting_outputs: int = 0  # Binary which outputs gave info
+        self.compare_inputs_full: int = 0  # Binary reported input nodes
+        self.compare_outputs_full: int = 0  # Binary output nodes
+        self.input_nodes: OrderedDict = OrderedDict()  # Ordered Dictionary
+        self.output_nodes: OrderedDict = OrderedDict()  # Ordered Dictionary
 
-        pass
-
-    # fixme
     @abstractmethod
     def process_new_input_node(self, node):
-        """TODO Docs"""
+        """
+        Abstract method used to process input nodes
 
-    # fixme
+        Args:
+            node: node object to be processed
+        """
+
     @abstractmethod
     def process_new_output_node(self, node):
-        """TODO DOCS"""
+        """
+        Abstract method used to process output nodes
 
-    # fixme
+        Args:
+            node: node object to be processed
+        """
+
     def clear_and_add_input_nodes(self, nodes: list):
-        """TODO DOcs"""
-        # TODO Break this up with helper methods
-        # TODO Clear entire dictionary of input nodes
-        # - Adjusting all internal variables
-        # TODO add each node in our list as a key in the input nodes dict,
-        #  with a value of None.
-        # TODO Run proccess_new_input_node() for each node, and again update
-        #  relevant variables (compare_input_full)
+        """
+        Method which clears the current input nodes and connects the new
+        list of nodes given.
 
-        pass
+        Clears input_nodes.
+        Sets input_connections to 0.
+        Sets reporting_inputs to 0.
+        Sets the input_nodes dictionary to the given list of nodes with key
+          values set to None.
+
+        Args:
+            nodes: list of nodes to be added
+        """
+
+        self.input_nodes.clear()
+        self.input_connections = 0
+        self.reporting_inputs = 0
+
+        for index, node in enumerate(nodes):
+            self.input_nodes[nodes[index]] = None
+            self.process_new_input_node(nodes[index])
+            self.input_connections += 1
+            # self.reporting_inputs = self.reporting_inputs | 2 ** node
+            self.compare_inputs_full = \
+                self.compare_inputs_full | 2 ** index
 
     def clear_and_add_output_nodes(self, nodes: list):
-        """TODO Docs"""
-        # TODO duplicate code from input nodes function
-        pass
+        """
+        Method which clears the current output nodes and connects the new
+        list of nodes given.
 
-    pass
+        Clears output_nodes.
+        Sets output_connections to 0.
+        Sets reporting_outputs to 0.
+        Sets the output_nodes dictionary to the given list of nodes with key
+          values set to None.
+
+        Args:
+            nodes: list of nodes to be added
+            """
+
+        self.output_nodes.clear()
+        self.output_connections = 0
+        self.reporting_outputs = 0
+
+        for index, node in enumerate(nodes):
+            self.output_nodes[nodes[index]] = None
+            self.process_new_output_node(nodes[index])
+            self.output_connections += 1
+            # self.reporting_outputs = self.reporting_outputs | 2 ** node
+            self.compare_outputs_full = \
+                self.compare_outputs_full | 2 ** index
 
 
 class LayerType(Enum):
-    """TODO DOCS"""
-    INPUT = auto()
-    HIDDEN = auto()
-    OUTPUT = auto()
+    """
+    Enum class which is used as a type-flag for different node classes.
+
+    INPUT = Input node/layer
+    HIDDEN = Hidden node/layer
+    OUTPUT = Output node/layer
+    """
+    INPUT = auto
+    HIDDEN = auto
+    OUTPUT = auto
 
 
-# FIXME
 class Neurode(MultiLinkNode):
-    """TODO DOCS"""
+    """
+    Inherited Class through MultiLinkNode
 
-    def __init__(self, my_type):
-        """TODO DOCS"""
+    Attributes:
+        value: Current value of the Neurode
+        my_type: Given LayerType
+
+    """
+
+    def __init__(self, my_type: Enum = LayerType.INPUT):
+        """
+        Inits Neurode with all class and inherited attributes initialized
+
+        Args:
+            my_type: LayerType, which indicates the type of layer this
+            neurode will be part of
+        """
         super().__init__()
         self.value = None  # current value of the Neurode
         self.my_type = my_type  # LayerType values: input, hidden, or output
-        # TODO value
-        # TODO my_type
 
     def get_value(self):
-        """TODO Docs"""
+        """
+        Getter method for value attribute
+
+        Returns:
+            Current value of the Neurode
+
+        """
         return self.value
 
-    def get_my_type(self):
-        """TODO Docs"""
+    def get_type(self):
+        """
+        Getter method for my_type attribute
+
+        Returns:
+            Current LayerType setting of my_type
+        """
         return self.my_type
 
     def process_new_input_node(self, node):
-        """TODO DOCS"""
+        """
+        Method which processes a node to be added to input connections
 
-        # TODO generate a random number between 0 and 1
-        # TODO Generate a new dictionary entry
-        pass
+        Generates a random weight, and then adds the node and the given
+          weight to the ordered dictionary input_nodes
+
+        Args:
+            node: given node
+        """
+        weight = random.uniform(0, 1)
+        self.input_nodes[node] = weight
 
     def process_new_output_node(self, node):
-        """TODO Docs"""
-        # Does nothing right now
+        """
+        Method which processes a node to be added to output connections
+
+        Implemented stub method
+        Args:
+            node: given node
+        """
         pass
+
+
+def main():
+    try:
+        bad_instance = MultiLinkNode()
+        print(
+            "Error: perhaps the abstract methods were not coded correctly in MultiLinkNode")
+    except TypeError:
+        pass
+    inodes = []
+    hnodes = []
+    onodes = []
+    for k in range(3):
+        inodes.append(Neurode(LayerType.INPUT))
+    for k in range(4):
+        hnodes.append(Neurode(LayerType.HIDDEN))
+
+    onodes.append(Neurode(LayerType.OUTPUT))
+    for node in inodes:
+        node.clear_and_add_output_nodes(hnodes)
+    for node in hnodes:
+        node.clear_and_add_input_nodes(inodes)
+        node.clear_and_add_output_nodes(onodes)
+    for node in onodes:
+        node.clear_and_add_input_nodes(hnodes)
+    onodes[0].value = 10
+    node_1 = list(inodes[2].output_nodes.items())[1][0]
+    node_2 = list(node_1.output_nodes.items())[0][0]
+    try:
+        assert inodes[0].get_type() == LayerType.INPUT
+        assert hnodes[0].get_type() == LayerType.HIDDEN
+        assert onodes[0].get_type() == LayerType.OUTPUT
+    except:
+        print("Error: perhaps LayerType labels were not applied correctly")
+    try:
+        assert node_2.get_value() == 10
+        assert 0 <= list(node_2.input_nodes.items())[1][1] < 1
+        inodes[2].value = 15
+        node_1 = list(onodes[0].input_nodes.items())[1][0]
+        node_2 = list(node_1.input_nodes.items())[2][0]
+        assert node_2.get_value() == 15
+    except:
+        print(
+            "Error: perhaps there is a problem linking the nodes, or with get_value()")
+    try:
+        assert hnodes[1].compare_inputs_full == 7
+        assert inodes[0].compare_outputs_full == 15
+    except:
+        print(
+            "Error: perhaps there is an error encoding inputs_full references")
+
+
+if __name__ == "__main__":
+    main()
